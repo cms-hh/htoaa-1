a1c8836bcac20656e0c5c36d448014aba111fa03
“saswatinandan” <saswati.nandan@cern.ch>
2023-05-02 09:47:41 +0200

    add individual bkg in plot

--- plotvar.py
+++ plotvar.py
@@ -10,18 +10,17 @@ file_='hadd.root'
 encoding = 'utf-8'
 for var in ['hLeadingFatJetParticleNetMD_Xbb_central', 'hLeadingFatJetDeepTagMD_bbvsLight_central']:
     d = uproot.open('hadd.root')['evt']
-    foundbkg = False
+    bkgs = []
+    errs = []
+    labels = []
     for key in d.keys():
         name = str(d[key].name, encoding)
         if name == 'TTJets': continue
-        if 'SUSY' not in name:
-            if not foundbkg:
-                bkg = d[f'{name}/{var}'].values
-                err2 = d[f'{name}/{var}'].variances
-                foundbkg = True
-            else:
-                bkg = np.sum((bkg, d[f'{name}/{var}'].values), axis=0)
-                err2 = np.sum((err2, d[f'{name}/{var}'].variances), axis=0)
+        if 'SUSY' in name: continue
+        value = d[f'{name}/{var}'].values
+        bkgs.append(value)
+        errs.append(np.sqrt(d[f'{name}/{var}'].variances))
+        labels.append(f'{name}: {np.sum(value):0,.2f}')
     for m in [12,15,20,25,30,35,40,45,50,55,60]:
         for key in d.keys():
             name = str(d[key].name, encoding)
@@ -31,7 +30,7 @@ for var in ['hLeadingFatJetParticleNetMD_Xbb_central', 'hLeadingFatJetDeepTagMD_
             plt.style.use([hep.style.CMS])
             fig, axis = plt.subplots()
             hep.histplot(hist.values, bins=hist.edges, ax=axis, yerr=np.sqrt(hist.variances), density=True, label=f'mass_{str(m)}')
-            hep.histplot(bkg, bins=hist.edges, ax=axis, yerr=np.sqrt(err2), density=True, label=f'bkg')
+            hep.histplot(bkgs, bins=hist.edges, ax=axis, yerr=errs, density=True, label=labels)
             axis.set_yscale('log', base=10)
             plt.legend()
             plt.tight_layout()
