68f4def7b14937e931afae5e67475ab8e7b67bae
“saswatinandan” <saswati.nandan@cern.ch>
2023-08-06 11:10:20 +0200

    add wildcard, rm unnecessary hadd files

--- hadd.py
+++ hadd.py
@@ -5,20 +5,25 @@ import argparse
 parser = argparse.ArgumentParser(description='htoaa analysis wrapper')
 parser.add_argument('-p', dest='input_path', type=str, required=True)
 parser.add_argument('-o', dest='output_file', type=str, required=True)
+parser.add_argument('-w', dest='wildcard', type=str, default='*root')
 args=parser.parse_args()
 
-p = args.input_path#'/afs/cern.ch/work/s/snandan/public/myforkhtoaa/htoaa-1/test_v2'
+p = args.input_path
 output_file = args.output_file
-files = glob.glob(f'{p}/*root')
+wc = args.wildcard
+
+files = glob.glob(f'{p}/{wc}')
 nfiles=5
 dohadd = True
 count = 0
 final_hadd = ''
+outputdir = output_file[:output_file.rfind('/')]
+rmfiles = []
 while dohadd:
     outputs = []
     for idx, f in enumerate(range(0, len(files), nfiles)):
         files_tohadd = ' '.join(files[f:f+nfiles])
-        output = f'hadd_{count}_{idx}.root' if len(files) > nfiles else output_file
+        output = os.path.join(outputdir, f'hadd_{count}_{idx}.root') if len(files) > nfiles else os.path.join(outputdir,output_file)
         outputs.append(output)
         cmd = f'hadd -f {output} {files_tohadd}'
         print('cmd: ', cmd)
@@ -29,4 +34,7 @@ while dohadd:
     else:
         files = outputs
         count += 1
-    
+        rmfiles.extend(outputs)
+
+for file_ in rmfiles:
+    os.system(f'rm {file_}')
