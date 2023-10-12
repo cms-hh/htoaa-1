dfab47f12b7d0720850a4cc458ee7a39a08e37cc
“saswatinandan” <saswati.nandan@cern.ch>
2023-10-12 10:05:00 +0200

    add run lumi event selectrion criteria

--- auxiliary.py
+++ auxiliary.py
@@ -1,6 +1,7 @@
 import awkward as ak
 import numpy as np
 from coffea.nanoevents.methods import vector
+from coffea.lumi_tools import LumiMask
 
 def met_p4(events):
     v= ak.zip(
@@ -31,3 +32,14 @@ def top_pT_reweighting(gen):
     top = gen[(gen.pdgId == 6) & gen.hasFlags(["isLastCopy"])]
     anti_top = gen[(gen.pdgId == -6) & gen.hasFlags(["isLastCopy"])]
     return np.sqrt(top_pT_sf_formula(top.pt) * top_pT_sf_formula(anti_top.pt))
+
+
+def selectRunLuminosityBlock(dataLSSelGoldenJSON, runNumber_list, luminosityBlock_list):
+    mask_run_ls = np.full(len(runNumber_list), False, dtype=bool)
+    for idx_, (r,ls) in enumerate(zip(runNumber_list, luminosityBlock_list)):
+        if str(r) in dataLSSelGoldenJSON.keys():
+            for ls_range in dataLSSelGoldenJSON[str(r)]:
+                if ls >= ls_range[0] and ls <= ls_range[1]:
+                    mask_run_ls[idx_] = True
+                    break
+    return mask_run_ls
