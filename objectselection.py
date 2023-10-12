400d89a407b9c6267993f7f0b1aa455bb4d9ecfb
“saswatinandan” <saswati.nandan@cern.ch>
2023-10-12 10:08:19 +0200

    some changes

--- objectselection.py
+++ objectselection.py
@@ -14,20 +14,8 @@ class ObjectSelection:
         self.FatJetMSoftDropThshLow  = 90
         self.FatJetMSoftDropThshHigh = 200
 
-        self.FatJetParticleNetMD_Xbb_Thsh = 0.8
-
         self.JetPtThshForHT = 30.0
         self.JetEtaThshForHT = 2.4
-
-        self.nFatJetMin = 1
-        self.GenHTThsh  = 100.0
-        self.LHEHTThsh  = 100.0
-
-        self.leadingelePtThsh = 26
-        self.leadingmuPtThsh = 26
-        self.leadingeleEtaThsh = 2.3
-        self.leadingmuEtaThsh = 2.4
-
 
     def selectGenHiggs(self, events):
         maskGenHiggs = (
@@ -54,41 +42,38 @@ class ObjectSelection:
     def selectFatJets(self, events):
         mask = ((events.FatJet.pt >= self.FatJetPtThsh)
                 & (abs(events.FatJet.eta) <= self.FatJetEtaThsh)
-                & (events.FatJet.msoftdrop >= self.FatJetMSoftDropThshLow)
-                #& (events.FatJet.deepTagMD_bbvsLight >= 0.98)
                 )
         return events.FatJet[mask]
 
     def selectMuons(self, events):
-        mask = ((events.Muon.pt >25) &
-                (abs(events.Muon.eta) < 2.4) &
-                (events.Muon.dxy < 0.05) &
-                (events.Muon.dz < 0.1 ) &
-                (events.Muon.miniPFRelIso_all < 0.4) &
-                (events.Muon.sip3d <8 )
+        mask = ((events.Muon.pt >25)
+                & (abs(events.Muon.eta) < 2.4)
+                & (events.Muon.dxy < 0.05)
+                & (events.Muon.dz < 0.1)
+                & (events.Muon.miniPFRelIso_all < 0.4)
+                & (events.Muon.sip3d <8 )
                 & (events.Muon.mediumId)
-                #& (events.Muon.mvaTTH >= 0.5)
-        )
+                & (events.Muon.mvaTTH >= -0.6)
+            )
 
         return events.Muon[mask]
 
     def selectElectrons(self, events):
-        mask = ((events.Electron.pt > 33) &
-                (abs(events.Electron.eta) < 2.3) &
-                (events.Electron.dxy < 0.05) &
-                (events.Electron.dz < 0.1 ) &
-                (events.Electron.miniPFRelIso_all < 0.4) &
-                (events.Electron.sip3d <8 )
+        mask = ((events.Electron.pt > 34)
+                & (abs(events.Electron.eta) < 2.3)
+                & (events.Electron.dxy < 0.05)
+                & (events.Electron.dz < 0.1)
+                & (events.Electron.miniPFRelIso_all < 0.4)
+                & (events.Electron.sip3d <8)
                 & (events.Electron.mvaFall17V2noIso_WP90)
-                #& (events.Electron.mvaTTH >= 0.3)
+                & (events.Electron.mvaTTH >= -0.6)
         )
 
         return events.Electron[mask]
 
     def selectak4Jets(self, events):
         mask = (
-            (events.Jet.pt > 20) &
-            (events.Jet.eta < 2.4) &
-            (events.Jet.btagDeepFlavB > 0.71)
+            (events.Jet.pt > 20)
+            & (events.Jet.eta < 2.4)
         )
         return events.Jet[mask]
