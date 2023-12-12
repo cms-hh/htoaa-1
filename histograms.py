f9f454d0699680c9d0ffc9451555d07487492278
“saswatinandan” <saswati.nandan@cern.ch>
2023-12-12 08:17:14 +0100

    add more histograms

--- histograms.py
+++ histograms.py
@@ -62,6 +62,14 @@ def histograms(systList, msoftdrop):
         .Reg(100,0.,300., name="hLeadingFatJetMSoftDrop", label="hLeadingFatJetMSoftDrop", overflow=True,underflow=True)
         .Weight()
     )
+    output['FatJet_particleNet_massH_Hto4b_v0'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(systList, name="systematic")
+        #.Variable([90, 95, 100., 105, 110, 115, 120, 125, 130, 140, 150, 170, 200, 800.], name="hLeadingFatJetMSoftDrop", label="hLeadingFatJetMSoftDrop") 
+        .Reg(100,0.,300., name="FatJet_particleNet_massH_Hto4b_v0", label="FatJet_particleNet_massH_Hto4b_v0", overflow=True,underflow=True)
+        .Weight()
+    )
     output['hLeadingFatJetParticleNetMD_Xbb'] = (
         hist.Hist.new
         .StrCat(msoftdrop, name="jet")
@@ -69,25 +77,11 @@ def histograms(systList, msoftdrop):
         .Reg(200, 0., 1, name="hLeadingFatJetParticleNetMD_Xbb", label="hLeadingFatJetParticleNetMD_Xbb", overflow=True,underflow=True)
         .Weight()
     )
-    output['hLeadingFatJetDeepTagMD_bbvsLight'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(systList, name="systematic")
-        .Reg(200, 0., 1, name="hLeadingFatJetDeepTagMD_bbvsLight", label="hLeadingFatJetDeepTagMD_bbvsLight", overflow=True,underflow=True)
-        .Weight()
-    )
     output['hLeadingFatJetPt'] = (
         hist.Hist.new
         .StrCat(msoftdrop, name="jet")
         .StrCat(systList, name="systematic")
         .Reg(200, 0, 1000, name="hLeadingFatJetPt", label="hLeadingFatJetPt", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['hLeadingFatJetMass'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(systList, name="systematic")
-        .Reg(300, 0, 300, name="hLeadingFatJetMass", label="hLeadingFatJetMass", overflow=True,underflow=True)
         .Weight()
     )
     output['LHE_HT_gen'] = (
@@ -139,32 +133,11 @@ def histograms(systList, msoftdrop):
         .Reg(10, -0.5, 9.5, name="ak4jetsize_mediumbtag", label="ak4jetsize_mediumbtag", overflow=True,underflow=True)
         .Weight()
     )
-    output['ak4jetsize_notcleaned'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(10, -0.5, 9.5, name="ak4jetsize_notcleaned", label="ak4jetsize", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['ak4jetsize_notcleaned_mediumbtag'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(10, -0.5, 9.5, name="ak4jetsize_notcleaned_mediumbtag", label="ak4jetsize_mediumbtag", overflow=True,underflow=True)
-        .Weight()
-    )
     output['dr_lep_ak4jet'] = (
         hist.Hist.new
         .StrCat(msoftdrop, name="jet")
         .StrCat(['central'], name="systematic")
         .Reg(50, 0., 5., name="dr_lep_ak4jet", label="dr_lep_ak4jet", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['tau3'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(50, 0., 1., name="tau3", label="tau3", overflow=True,underflow=True)
         .Weight()
     )
     output['tau4_2'] = (
@@ -181,67 +154,32 @@ def histograms(systList, msoftdrop):
         .Reg(50, 0., 1., name="tau4_3", label="tau4_3", overflow=True,underflow=True)
         .Weight()
     )
-    output['nConstituents'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(100, 0., 100., name="nConstituents", label="nConstituents", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['subjetpt'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(100, 0., 500., name="subjetpt", label="subjetpt", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['subjet2pt'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(100, 0., 500., name="subjet2pt", label="subjet2pt", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['subjet1_btag'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(100, 0., 1., name="subjet1_btag", label="subjet1_btag", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['subjet2_btag'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(100, 0., 1., name="subjet2_btag", label="subjet2_btag", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['subjet1_nBhadrons'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(5, 0., 5., name="subjet1_nBhadrons", label="subjet1_nBhadrons", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['subjet2_nBhadrons'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(5, 0., 5., name="subjet2_nBhadrons", label="subjet2_nBhadrons", overflow=True,underflow=True)
-        .Weight()
-    )
-    output['deltar'] = (
-        hist.Hist.new
-        .StrCat(msoftdrop, name="jet")
-        .StrCat(['central'], name="systematic")
-        .Reg(25, 0., 0.85, name="deltar", label="deltar", overflow=True,underflow=True)
-        .Weight()
-    )
     output['dq_1'] = (
         hist.Hist.new
         .StrCat(msoftdrop, name="jet")
         .StrCat(['central'], name="systematic")
         .Reg(50, 0, 5, name="dq_1", label="dq_1", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['dphi_lm'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(50, -3.14, 3.14, name="dphi_lm", label="dphi_lm", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['dphi_lm_jet'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(50, -3.14, 3.14, name="dphi_lm_jet", label="dphi_lm_jet", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['sign_deta'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(50, -3.14, 3.14, name="sign_deta", label="sign_deta", overflow=True,underflow=True)
         .Weight()
     )
     output['db_1'] = (
@@ -270,6 +208,153 @@ def histograms(systList, msoftdrop):
         .StrCat(msoftdrop, name="jet")
         .StrCat(['central'], name="systematic")
         .Reg(5, 0.5, 5.5, name="ak8jetsize", label="ak8jetsize", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_Haa3b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_Haa3b", label="FatJet_particleNetMD_Hto4b_Haa3b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_Haa4b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_Haa4b", label="FatJet_particleNetMD_Hto4b_Haa4b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_Haa34b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_Haa34b", label="FatJet_particleNetMD_Hto4b_Haa34b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_Haa2b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_Haa2b", label="FatJet_particleNetMD_Hto4b_Haa2b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_Haa01b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_Haa01b", label="FatJet_particleNetMD_Hto4b_Haa01b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_QCD4b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_QCD4b", label="FatJet_particleNetMD_Hto4b_QCD4b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_QCD3b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_QCD3b", label="FatJet_particleNetMD_Hto4b_QCD3b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_QCD2b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_QCD2b", label="FatJet_particleNetMD_Hto4b_QCD2b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_QCD1b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_QCD1b", label="FatJet_particleNetMD_Hto4b_QCD1b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_QCD0b'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_QCD0b", label="FatJet_particleNetMD_Hto4b_QCD0b", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNet_massA_Hto4b_v1'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(200, 0., 80, name="FatJet_particleNet_massA_Hto4b_v1", label="FatJet_particleNet_massA_Hto4b_v1", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNet_massA_Hto4b_v2'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(200, 0., 80., name="FatJet_particleNet_massA_Hto4b_v2", label="FatJet_particleNet_massA_Hto4b_v2", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNet_massA_Hto4b_v3'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(200, 0., 80., name="FatJet_particleNet_massA_Hto4b_v3", label="FatJet_particleNet_massA_Hto4b_v3", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNet_massA_Hto4b_v0'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(200, 0., 80., name="FatJet_particleNet_massA_Hto4b_v0", label="FatJet_particleNet_massA_Hto4b_v0", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNet_massH_Hto4b_v3'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 50., 200., name="FatJet_particleNet_massH_Hto4b_v3", label="FatJet_particleNet_massH_Hto4b_v3", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNet_massH_Hto4b_v2'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 50., 200., name="FatJet_particleNet_massH_Hto4b_v2", label="FatJet_particleNet_massH_Hto4b_v2", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNet_massH_Hto4b_v1'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 50., 200., name="FatJet_particleNet_massH_Hto4b_v1", label="FatJet_particleNet_massH_Hto4b_v1", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNet_massH_Hto4b_v0'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 50., 200., name="FatJet_particleNet_massH_Hto4b_v0", label="FatJet_particleNet_massH_Hto4b_v0", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto4b_vs_QCDsum'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto4b_vs_QCDsum", label="FatJet_particleNetMD_Hto4b_vs_QCDsum", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['FatJet_particleNetMD_Hto34b_vs_QCDsum'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 1., name="FatJet_particleNetMD_Hto34b_vs_QCDsum", label="FatJet_particleNetMD_Hto34b_vs_QCDsum", overflow=True,underflow=True)
+        .Weight()
+    )
+    output['apt'] = (
+        hist.Hist.new
+        .StrCat(msoftdrop, name="jet")
+        .StrCat(['central'], name="systematic")
+        .Reg(100, 0., 500., name="pt", label="a_pt", overflow=True,underflow=True)
         .Weight()
     )
     return output
